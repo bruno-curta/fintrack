a63d52c1144d2be1d8cb2bd2302bb0f1283e874c
bruno-curta <curtarelli@gmail.com>
2025-04-06 15:26:18 -0300

    melhorando o tracking

--- app.py
+++ app.py
@@ -151,15 +151,93 @@ def server(input: Inputs, output: Outputs, session: Session):
         ano = int(input.year())
         mes = int(input.month())
         df_final = df_track.loc[(df_track['year']==ano) & (df_track['month']==mes), ['category', 'value', 'target', 'track']]
+                
+        start = pd.to_datetime('now')+pd.tseries.offsets.MonthEnd(-1)-pd.DateOffset(months=3)
+        end = pd.to_datetime('now')+pd.tseries.offsets.MonthEnd(-1)
+
+        def max_day(year, month):
+            if month in [1, 3, 5, 7, 8, 10, 12]:
+                return 'percent_acumulado_31'
+            elif month in [4, 6, 9, 11]:
+                return 'percent_acumulado_30'
+            else:
+                if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+                    return 'percent_acumulado_29'
+                else:
+                    return 'percent_acumulado_28'
+                
+        # curvas de gasto acumulado por categoria
+        df_day = df.query("@start <= track_timestamp <= @end").groupby(['category','day'])['value'].sum().reset_index()
+        df_day['num_months'] = df.query("@start <= track_timestamp <= @end")['month'].nunique()
+        df_day['value'] = df_day['value']/df_day['num_months']
+        c, d = pd.core.reshape.util.cartesian_product([pd.unique(df_day['category']), list(range(1,32))])
+        df_all_day = pd.DataFrame(dict(category=c, day=d))
+        df_day = pd.merge(df_all_day, df_day, on=['category','day'], how='left')
+        df_day['value'] = df_day['value'].fillna(0)
+        df_day['cum_value']=df_day.groupby('category')['value'].cumsum()
+
+        df_day['percent_acumulado_28']=df_day.query('day<=28')['cum_value']/df_day.query('day<=28').groupby('category')['cum_value'].transform('max')
+        df_day['percent_acumulado_29']=df_day.query('day<=29')['cum_value']/df_day.query('day<=29').groupby('category')['cum_value'].transform('max')
+        df_day['percent_acumulado_30']=df_day.query('day<=30')['cum_value']/df_day.query('day<=30').groupby('category')['cum_value'].transform('max')
+        df_day['percent_acumulado_31']=df_day.query('day<=31')['cum_value']/df_day.query('day<=31').groupby('category')['cum_value'].transform('max')
+
+        df_day['expected'] = round(df_day[max_day(ano, mes)]*100)
+                
+        # curvas de gasto acumulado total
+        df_esperado_total = df.query("@start <= track_timestamp <= @end").groupby(['day'])['value'].sum().reset_index()
+        df_esperado_total['num_months'] = df.query("@start <= track_timestamp <= @end")['month'].nunique()
+        df_esperado_total['value'] = df_esperado_total['value']/df_esperado_total['num_months']
+        df_all_day = pd.DataFrame({'day':list(range(1,32))})
+        df_esperado_total = pd.merge(df_all_day, df_esperado_total, on=['day'], how='left')
+        df_esperado_total['value'] = df_esperado_total['value'].fillna(0)
+        df_esperado_total['cum_value']=df_esperado_total['value'].cumsum()
+
+        df_esperado_total['percent_acumulado_28']=df_esperado_total.query('day<=28')['cum_value']/df_esperado_total.query('day<=28')['cum_value'].max()
+        df_esperado_total['percent_acumulado_29']=df_esperado_total.query('day<=29')['cum_value']/df_esperado_total.query('day<=29')['cum_value'].max()
+        df_esperado_total['percent_acumulado_30']=df_esperado_total.query('day<=30')['cum_value']/df_esperado_total.query('day<=30')['cum_value'].max()
+        df_esperado_total['percent_acumulado_31']=df_esperado_total.query('day<=31')['cum_value']/df_esperado_total.query('day<=31')['cum_value'].max()
+
+        df_esperado_total['category'] = 'Total'
+        df_esperado_total['value'] = df_esperado_total['cum_value']
+        df_esperado_total['target'] = 13700
+        df_esperado_total['expected'] = round(df_esperado_total[max_day(ano, mes)]*100)
         
-        def highlight_SLA(series):
+        #filter df_day with the last actual day of the month from the df_final table
+        last_day = df.query('year==@ano & month==@mes')['day'].max()
+
+        df_esperado_total = df_esperado_total.query('day==@last_day')
+        df_day = df_day.query('day==@last_day')
+        df_esperado_total = pd.concat([df_esperado_total[['category', 'expected']], df_day[['category', 'expected']]], ignore_index=True)
+        
+        df_final = pd.merge(df_final, df_esperado_total, on='category', how='right')
+
+
+        def highlight_SLA(row):
             green = 'background-color: green'
             yellow = 'background-color: yellow'
             pink = 'background-color: red'
-            return [green if value <= 95 else yellow if value <100 else pink for value in series]
-        
-        return ui.HTML(df_final.style.hide().apply(highlight_SLA, subset=['track']).format('{:.0f}', na_rep='MISS', subset=['value', 'target', 'track']).to_html(index=False))
 
+            styles = [''] * len(row)  # Default empty styles for all columns
+
+            if row['track'] <= row['expected'] * 0.95:
+                styles[row.index.get_loc('track')] = green
+                styles[row.index.get_loc('expected')] = green
+            elif row['track'] <= row['expected']:
+                styles[row.index.get_loc('track')] = yellow
+                styles[row.index.get_loc('expected')] = yellow
+            else:
+                styles[row.index.get_loc('track')] = pink
+                styles[row.index.get_loc('expected')] = pink
+
+            return styles
+        
+        return ui.HTML(
+                    df_final.style.hide()
+                    .apply(highlight_SLA, axis=1)  # Apply row-wise
+                    .format('{:.0f}', na_rep='MISS', subset=['value', 'target', 'track', 'expected'])
+                    .to_html(index=False)
+                )
+    
      # Define the chat
     chat = ui.Chat(id="chat")
 
